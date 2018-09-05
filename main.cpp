2ca3773ed646ea8c29beec012c610b2070f1e4e5
smeridian387 <43002188+smeridian387@users.noreply.github.com>
2018-09-05 15:46:23 +0100

    Creating game window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
+//included library section
+
+#include <SFML/Graphics.hpp>
+
 
 //entry point for the program
 int main()
 {
+	// make a variable called a gamewindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode(800, 600), "button masher");
 
+	//game loop
 	while (true)
 	{
-		//game loop
+		// TODO: Check for input
+
+		// TODO: update game state
+
+		// TODO: Draw graphics 
 
 	}
 
